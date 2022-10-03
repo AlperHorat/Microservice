b46ae04e1d9dfb3e6a7d048543c21d5eb6d15962
AlperHorat <114068365+AlperHorat@users.noreply.github.com>
2022-10-03 17:16:15 +0300

    ContactManager seviyesinde unit testlerin yapılması

---
+++ Microservice/Microservice.Tests/ContactManagerTests.cs
@@ -0,0 +1,91 @@
+﻿using ContactService.Api.Context;
+using ContactService.Api.Domain.Entities;
+using ContactService.Api.Managers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Microservice.Tests
+{
+    public class ContactManagerTests
+    {
+        private readonly ContactManager _manager;
+
+        public ContactManagerTests()
+        {
+            var options = new DbContextOptionsBuilder<PostgreSqlDbContext>().UseNpgsql(Values.ContactConString).Options;
+
+            var _context = new PostgreSqlDbContext(options);
+            _manager = new ContactManager(_context);
+        }
+        [Fact]
+        public void GetPersons_NotNullControl()
+        {
+            // Act
+            var result = _manager.GetPersons();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void AddPerson_ValidObjectPassed_ReturnsTrue()
+        {
+            // Arrange
+            Person entity = new Person();
+            entity.Id = Guid.NewGuid();
+            entity.Name = "test";
+            entity.Firm = null;
+            // Act
+            var response = _manager.AddPerson(entity);
+
+            // Assert
+            Assert.True(true.Equals(response));
+        }
+        [Fact]
+        public void GetPersonById_InvalidObjectPassed_ReturnsNull()
+        {
+            // Arrange
+            Guid id = new Guid();
+            // Act
+            var response = _manager.GetPersonById(id);
+
+            // Assert
+            Assert.Null(response);
+        }
+        [Fact]
+        public void DeletePerson_InvalidObjectPassed_ReturnsFalse()
+        {
+            // Arrange
+            Guid id = new Guid();
+            // Act
+            var response = _manager.DeletePerson(id);
+
+            // Assert
+            Assert.True(false.Equals(response));
+        }
+        [Fact]
+        public void DeletePersonContactInfo_InvalidObjectPassed_ReturnsFalse()
+        {
+            // Arrange
+            Guid id = new Guid();
+            // Act
+            var response = _manager.DeletePersonContactInfo(id);
+
+            // Assert
+            Assert.True(false.Equals(response));
+        }
+        [Fact]
+        public void GetPersonContacts_InvalidObjectPassed_ReturnsNull()
+        {
+            // Arrange
+            Guid personid = new Guid();
+            // Act
+            var response = _manager.GetPersonContacts(personid);
+
+            // Assert
+            Assert.Null(response);
+        }
+    }
+}
